87975a5ecd91b332d78c46713080f8d521a9f164
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-29 09:51:41 +0700

    feat: change views from register to login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Register from "@/pages/Register"
+// import Register from "@/pages/Register"
 
-// import Login from "@/pages/Login"
+import Login from "@/pages/Login"
 
 function App() {
   return (
    <>
-    <Register />
-    {/* <Login /> */}
+    {/* <Register /> */}
+    <Login />
    </>
   )
 }
