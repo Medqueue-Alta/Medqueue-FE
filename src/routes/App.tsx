26d9418b5cbf26d412771b0b391b06e3c5c74939
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-29 10:42:29 +0700

    feat: add pages router

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,3 +1,7 @@
+import FaskesAddSchedule from '@/pages/FaskesAddSchedule'
+import FaskesHome from '@/pages/FaskesHome'
+import FaskesQueue from '@/pages/FaskesQueue'
+import FaskesSchedule from '@/pages/FaskesSchedule'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
@@ -11,6 +15,22 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />
+  },
+  {
+    path: "/",
+    element: <FaskesHome />
+  }, 
+  {
+    path: "/faskes/antrian",
+    element: <FaskesQueue />
+  }, 
+  {
+    path: "/faskes/jadwal",
+    element: <FaskesSchedule />
+  },
+  {
+    path: "/faskes/jadwal/add",
+    element: <FaskesAddSchedule />
   }
 ])
 
