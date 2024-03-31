149b6db2b154071f93b0ade42c62424b5a1f802a
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-03-31 17:37:55 +0700

    feat: add patient infomation card

---
+++ src/components/PatientInformationCard.tsx
@@ -0,0 +1,33 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface Props {
+  nama: string;
+  NIK: string;
+  BJPS: string;
+}
+
+const PatientInformationCard = (props: Props) => {
+  const { nama, NIK, BJPS } = props;
+
+  return (
+    <Card className=" w-max h-1/2 ">
+      <CardHeader className="flex flex-row justify-center bg-[#92DBD8]">
+        <CardTitle>{nama}</CardTitle>
+      </CardHeader>
+      <CardContent className="p-4 bg-[#BEE8E6]">
+        <div className="grid grid-cols-3">
+          <p>NIK</p>
+          <p>:</p>
+          <p className="">{NIK}</p>
+        </div>
+        <div className="grid grid-cols-3">
+          <p>BPJS</p> 
+          <p>:</p>
+          <p>{BJPS}</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PatientInformationCard;
