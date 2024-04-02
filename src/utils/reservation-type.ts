80c6039f0e30fa5c8ba052e53f2d0fe109855af3
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-04-03 01:01:33 +0700

    chores: update schema to store Jadwal as string instead date

--- src/utils/reservation-type.ts
+++ src/utils/reservation-type.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const reservationSchema = z.object({
   Poli: z.string().min(1, { message: "Please Choose The Clinic" }),
   Hari: z.date({ required_error: "Please Choose The Date" }),
-  Jadwal: z.date({ required_error: "Please Choose The Schedule" }),
+  Jadwal: z.string().min(1, { message: "Please Choose The Schedule" }),
   Keluhan: z.string().min(1, { message: "Please Enter Your Symptoms" }),
   BPJS: z.boolean().default(false).optional(),
 });
