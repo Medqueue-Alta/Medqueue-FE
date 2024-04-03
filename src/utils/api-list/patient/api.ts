054f1452a5a0afa01bd4aec804ebdd1dfae89500
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-04-03 13:25:18 +0700

    feat: added api for the first time

---
+++ src/utils/api-list/patient/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+import { checkProperty, valueFormatData } from "@/utils/formatter";
+import { IResponse } from "@/utils/types/api";
+import { ReservationSchema } from "./reservation-type";
+
+export const addNewReservation = async (body: ReservationSchema) => {
+  try {
+    const formData = new FormData();
+
+    let key: keyof typeof body;
+    for (key in body) {
+      if (checkProperty(body[key])) {
+        formData.append(key, valueFormatData(body[key]));
+      }
+    }
+
+    const response = await axios.post("/books", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data as IResponse;
+  } catch (error: any) {
+    throw Error(error.response.data.message);
+  }
+};
