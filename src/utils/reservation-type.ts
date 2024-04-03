429d89747d0c78da55e3f4152dcfd48f120e2d8c
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-04-03 12:35:45 +0700

    chores: updating reservation's schema for "Keluhan"

--- src/utils/reservation-type.ts
+++ src/utils/reservation-type.ts
@@ -1,10 +1,17 @@
 import * as z from "zod";
 
 export const reservationSchema = z.object({
-  Poli: z.string().min(1, { message: "Please Choose The Clinic" }),
-  Hari: z.date({ required_error: "Please Choose The Date" }),
-  Jadwal: z.string().min(1, { message: "Please Choose The Schedule" }),
-  Keluhan: z.string().min(1, { message: "Please Enter Your Symptoms" }),
+  Poli: z.string().min(1, { message: "Please choose the clinic" }),
+  Hari: z.date({ required_error: "Please choose the date" }),
+  Jadwal: z.string().min(1, { message: "Please choose the schedule" }),
+  Keluhan: z
+    .string()
+    .min(10, {
+      message: "Symptoms Must Be at least 10 characters.",
+    })
+    .max(160, {
+      message: "Symptoms must not be longer than 30 characters.",
+    }),
   BPJS: z.boolean().default(false).optional(),
 });
   
