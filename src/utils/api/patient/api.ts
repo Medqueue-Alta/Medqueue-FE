aafaece41613403102a7f82d9004691c754b368c
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-04-05 11:50:46 +0700

    feat: added api function and types for getting patient profile info

--- src/utils/api/patient/api.ts
+++ src/utils/api/patient/api.ts
@@ -3,6 +3,20 @@ import axios from "axios";
 import { checkProperty, valueFormatData } from "@/utils/formatter";
 import { IResponse } from "@/utils/types/api";
 import { ReservationSchema } from "./reservation-type";
+// import axiosWithConfig from "../axiosWithConfig";
+import { IPatient } from "./type";
+
+export const getPatient = async () => {
+  try {
+    const response = await axios.get(
+      "https://virtserver.swaggerhub.com/MNFAWWAZ99/MedQueue/1.0.0/profile"
+    );
+
+    return response.data as IPatient;
+  } catch (error: any) {
+    throw Error(error.response.data.message);
+  }
+};
 
 export const addNewReservation = async (body: ReservationSchema) => {
   try {
