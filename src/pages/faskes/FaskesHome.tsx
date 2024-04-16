b35a3bce2763a64bab59313ce44830da4453a8c3
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-04-16 13:49:00 +0700

    feat: add logout function

--- src/pages/faskes/FaskesHome.tsx
+++ src/pages/faskes/FaskesHome.tsx
@@ -1,9 +1,11 @@
 import FaskesLayout from "@/components/FaskesLayout"
 import FaskesSidebar from "@/components/FaskesSidebar"
 import Vector from "@/assets/vector.png"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { Separator } from "@/components/ui/separator"
 
 const FaskesHome = () => {
+    const navigate = useNavigate()
   return (
    <FaskesLayout>
         <FaskesSidebar>
@@ -18,6 +20,13 @@ const FaskesHome = () => {
                     <li className="text-white text-xl ">Setting Jadwal</li>
                 </div>
             </Link>
+            <Separator />
+            <div className="cursor-pointer p-2 max-w-[90%] rounded-lg" onClick={() => {
+                localStorage.clear()
+                navigate("/login")
+            }}>
+                <li className="text-white text-xl hover:text-red-500 duration-500">Logout</li>
+            </div>
         </ul>
         </FaskesSidebar>
         <div className="ml-5">
